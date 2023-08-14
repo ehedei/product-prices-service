b679492a07c44cdfa9822d47b95672dce3213ab9
Ehedei <ehedeidev@gmail.com>
2023-08-14 19:14:39 +0200

    Project base scaffolding

---
+++ src/main/java/dev/ehedei/product/prices/service/ProductPricesServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ehedei.product.prices.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPricesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPricesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ehedei/product/prices/service/ProductPricesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ehedei.product.prices.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPricesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
