6bd757d4354e76784aa9da8e3a60b746211c7a79
Ehedei <ehedeidev@gmail.com>
2023-08-18 14:16:04 +0200

    ProductPriceRepository updated with custom query

--- src/main/java/dev/ehedei/product/prices/service/repositories/ProductPriceRepository.java
+++ src/main/java/dev/ehedei/product/prices/service/repositories/ProductPriceRepository.java
@@ -3,5 +3,11 @@ package dev.ehedei.product.prices.service.repositories;
 import dev.ehedei.product.prices.service.domain.ProductPriceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface ProductPriceRepository extends JpaRepository<ProductPriceModel, Long> {
+    Optional<ProductPriceModel> findFirstByProductIdAndBrand_idAndStartDateBeforeAndEndDateAfterOrderByPriorityDesc(
+            Long productId, Long brand, LocalDateTime startDate, LocalDateTime endDate);
+
 }
--- src/main/java/dev/ehedei/product/prices/service/services/impl/ProductPriceServiceImpl.java
+++ src/main/java/dev/ehedei/product/prices/service/services/impl/ProductPriceServiceImpl.java
@@ -1,5 +1,6 @@
 package dev.ehedei.product.prices.service.services.impl;
 
+import dev.ehedei.product.prices.service.domain.ProductPriceModel;
 import dev.ehedei.product.prices.service.dtos.PriceSearchCriteriaDto;
 import dev.ehedei.product.prices.service.dtos.ProductPriceDto;
 import dev.ehedei.product.prices.service.repositories.ProductPriceRepository;
@@ -9,16 +10,43 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
-@Getter @Setter @AllArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class ProductPriceServiceImpl implements ProductPriceService {
 
     private ProductPriceRepository productPriceRepository;
 
     @Override
     public Optional<ProductPriceDto> getProductPriceBySearchCriteria(final PriceSearchCriteriaDto productPriceCriteria) {
-        return Optional.empty();
+
+        final Optional<ProductPriceModel> productPriceModel = getProductPriceRepository()
+                .findFirstByProductIdAndBrand_idAndStartDateBeforeAndEndDateAfterOrderByPriorityDesc(
+                        productPriceCriteria.getProductId(),
+                        productPriceCriteria.getBrandId(),
+                        productPriceCriteria.getDate(),
+                        productPriceCriteria.getDate()
+                );
+
+        return productPriceModel.map(this::convertProductPriceModelIntoDto);
+    }
+
+    protected ProductPriceDto convertProductPriceModelIntoDto(final ProductPriceModel productPriceModel) {
+        final ProductPriceDto productPriceDto = new ProductPriceDto();
+        productPriceDto.setProductId(productPriceModel.getProductId());
+        productPriceDto.setPrice(productPriceModel.getPrice());
+        productPriceDto.setPriceList(productPriceModel.getPriceList());
+        productPriceDto.setStartDate(productPriceModel.getStartDate());
+        productPriceDto.setEndDate(productPriceModel.getEndDate());
+
+        if (Objects.nonNull(productPriceModel.getBrand())) {
+            productPriceDto.setBrandId(productPriceModel.getBrand().getId());
+        }
+
+        return productPriceDto;
     }
 }
